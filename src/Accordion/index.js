2fbdecd5fbc414b7ff0c4019113159650c4ce142
swapnichavan <swapnil.chawan30@gmail.com>
2026-07-19 23:53:25 +0530

    PackingList and Accordion App

---
+++ src/Accordion/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import './accordion.css'
+
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.'
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.'
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!'
+  }
+]
+
+function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  )
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      <AccordionItem data={data} />
+    </div>
+  )
+}
+
+function AccordionItem({ data }) {
+  const [open, setOpen] = useState(false)
+  const [selected, setSelected] = useState(null)
+  console.log(selected)
+
+  const onHandleOpenItem = (id) => {
+    setSelected(id)
+    if (selected === id) {
+      setOpen((open) => !open)
+    } else {
+      setOpen(true)
+    }
+    // setOpen(!open)
+  }
+  return (
+    <>
+      {data.map((item, index) => (
+        <div
+          className={`item ${open && selected == index ? 'open' : ''}`}
+          key={index + 1}
+          onClick={() => onHandleOpenItem(index)}
+        >
+          <p className="number">{index >= 0 && `0${index + 1}`}</p>
+          <p className="title">{item.title}</p>
+          <p className="icon">{open && selected === index ? '+' : '-'}</p>
+          {open && selected === index ? (
+            <div className="content-box">{item.text}</div>
+          ) : null}
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default App
+useState, import React from 'react'
--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,7 @@
-import React, { useState } from 'react'
-import './travalList.css'
-import Logo from './Logo'
-import Form from './Form'
-import PackingList from './PackingList'
-import Stats from './Stats'
-
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: true },
-  { id: 2, description: 'Socks', quantity: 12, packed: false }
-]
+import React from 'react'
 
 function App() {
-  const [items, setItems] = useState([])
-  // const numItems = items.length
-
-  const onHandleAddItem = (item) => {
-    setItems((items) => [...items, item])
-  }
-
-  const onHandleDeleteItem = (id) => {
-    console.log(id)
-    setItems((items) => items.filter((item) => item.id !== id))
-  }
-
-  const onHandleToggle = (id) => {
-    setItems((items) =>
-      items.map((item) => {
-        if (item.id === id) {
-          return { ...item, packed: !item.packed }
-        } else {
-          return item
-        }
-      })
-    )
-  }
-
-  const onHandleClearList = () => {
-    setItems([])
-  }
-
-  return (
-    <div>
-      <Logo />
-      <Form onAddItem={onHandleAddItem} />
-      <PackingList
-        items={items}
-        onDeleteItem={onHandleDeleteItem}
-        onToggle={onHandleToggle}
-        onClearList={onHandleClearList}
-      />
-      <Stats items={items} />
-    </div>
-  )
+  return <div>App</div>
 }
 
 export default App
